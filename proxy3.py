5b238e61beb2dbf98f703ee1fc3902b1737543cb
bigsnarfdude <ohprecio@gmail.com>
2012-11-14 17:35:54 -0500

    dj told me so

--- proxy3.py
+++ proxy3.py
@@ -1,29 +1,43 @@
+#! /usr/bin/env python
+"""
+This program is a proxy server and holds cached webpages
+"""
+
 from twisted.web.client import getPage
 from twisted.internet import reactor, protocol, endpoints
 from twisted.protocols import basic
 from twisted.internet import defer
 
-import time
+class CacheServerProxyProtocol(basic.LineReceiver):
+    
+    def __init__(self, cache={}):
+        self.cache = cache
 
-class ProxyProtocol(basic.LineReceiver):
     def lineReceived(self, line):
         if not line.startswith("http://"):
             return
-        self.getPage(line)
+        
+        if line in self.cache:
+            page = self.cache[line]
+            return self.results_to_client(page)
 
-    @defer.inlineCallbacks
-    def getPage(self, url):
-        starttime = time.time()
-        print "Fetching", url
-        page = yield getPage(url)
-        print "Fetched", url
+        else:
+            result = getPage(line)
+            return self.results_to_client(result)
+    
+    @addCallback.inline
+    def addCache(self, url, page):
+        self.cache[url] = page
+        return page
+    
+    @addCallback.inline
+    def results_to_client(self, page):
         self.transport.write(page)
         self.transport.loseConnection()
-        print "Took", time.time() - starttime
 
-
+         
 factory = protocol.ServerFactory()
-factory.protocol = ProxyProtocol
+factory.protocol = CacheServerProxyProtocol()
 
 endpoints.serverFromString(reactor, "tcp:8000").listen(factory)
 reactor.run()
