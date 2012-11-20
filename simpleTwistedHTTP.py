bd1253e2e852e7a49bc4bdaecf63d5044ac2e02e
bigsnarfdude <ohprecio@gmail.com>
2012-11-20 11:44:35 -0500

    pep8 checked

--- simpleTwistedHTTP.py
+++ simpleTwistedHTTP.py
@@ -3,54 +3,47 @@
 This program is a simple webserver built with Twisted
 """
 
+
 from twisted.internet import reactor, protocol, endpoints
 from twisted.protocols import basic
 from twisted.web import client
-
 import time
 
-class WebserverProtocol(basic.LineReceiver):
-    
-    def lineReceived(self, line):
 
+class WebserverProtocol(basic.LineReceiver):
+
+    def lineReceived(self, line):
         if line.startswith("GET"):
-            # NOTE: I dont know how to catch and release this
             print "this line is firing inside GET"
             self._get_or_head(line)
             self.transport.write("this line is firing")
 
-
-    def _get_or_head(self, request_string): #is this **kwargs?
-
+    def _get_or_head(self, request_string):
         request_method = request_string.split(' ')[0]
         print "Method: ", request_method
         print "Request body: ", request_string
 
-        if (request_method == "GET") | (request_method == "HEAD"):
+        if request_method == "GET" or request_method == "HEAD":
             file_requested = request_string.split(" ")
             file_requested = file_requested[1]
-
             file_requested = file_requested.split("?")[0]
 
-            if (file_requested == "/"):
+            if file_requested == "/":
                 file_requested = "/index.html"
 
             file_requested = "/Users/antigen/dev/pythonNetworkProgrammingN00B/" + file_requested
-            print "Serving web page [",file_requested,"]"
-
+            print "Serving web page [", file_requested, "]"
 
     def _generate_headers(self, code):
-
         header_server = ""
-        if (code == 200):
+        if code == 200:
             header_server = "HTTP/1.1 200 OK\n"
-        elif(code == 404):
+        elif code == 404:
             header_server = "HTTP/1.1 404 Not Found\n"
         current_date = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
         header_server += "Date: " + current_date + "\n"
         header_server += "Server: HTTPTwisted-Server\n"
-        header_server += "Connection: close\n\n"  
-        
+        header_server += "Connection: close\n\n"
         return header_server
 
 """u'GET /favicon.ico HTTP/1.1\r\nHost: localhost:8888\r\nConnection: keep-alive\r\nAccept: */*\r\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11\r\nAccept-Encoding: gzip,deflate,sdch\r\nAccept-Language: en-US,en;q=0.8\r\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\r\n\r\n'"""
