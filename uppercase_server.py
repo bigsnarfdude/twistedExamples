c970fadb6aebe59b8a8e91cf056cae3ae3f9c466
bigsnarfdude <ohprecio@gmail.com>
2012-11-20 11:47:35 -0500

    pep8 checked

--- uppercase_server.py
+++ uppercase_server.py
@@ -2,11 +2,10 @@ from twisted.internet import reactor, protocol, endpoints
 
 
 class UpperProtocol(protocol.Protocol):
-    
 
     def connectionMade(self):
         self.factory.count += 1
-        self.transport.write("Hi! Send me text to convert to uppercase, count = %s \n" % self.factory.count)
+        self.transport.write("Hi! Count = %s \n" % self.factory.count)
 
     def connectionLost(self, reason):
         self.factory.count -= 1
